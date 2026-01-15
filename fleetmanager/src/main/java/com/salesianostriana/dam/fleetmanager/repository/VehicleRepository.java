ed8ee17a78394053d67dd382cc5877eab496be94
Alvaro Lázaro Castellón <alvarolazaro101@gmail.com>
2026-01-15 14:29:58 +0100

    Cambios menores

--- fleetmanager/src/main/java/com/salesianostriana/dam/fleetmanager/repository/VehicleRepository.java
+++ fleetmanager/src/main/java/com/salesianostriana/dam/fleetmanager/repository/VehicleRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface VehicleRepository
         extends JpaRepository<Vehicle, Long> {
 
